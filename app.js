1f8869d752681acb5c3ec01f4a8135c989b02c30
sunshine274 <jingx274@gmail.com>
2020-08-10 11:18:40 +0000

    Added move down and freeze functions

--- app.js
+++ app.js
@@ -68,9 +68,24 @@ function undraw() {
 // Make the tetromino move down every second
 timerId = setInterval(moveDown, 1000);
 
-// 
+// move down function
+function moveDown () {
+    undraw();
+    currentPosition += width;
+    draw();
+}
 
-
+//freeze function
+function freeze(){
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //start a new tetromino falling
+        random = Math.floor(Math.random() * theTetrominoes.length)
+        current = theTetrominoes[random][currentRotation]
+        currentPosition = 4;
+        draw()
+    }
+}
 
 
 });
